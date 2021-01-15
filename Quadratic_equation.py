0f50c25b817653f85a131c5cff37a4620dadf811
Hamid.haghshenas <h.hagshenas@hotmail.com>
2021-01-15 12:52:34 +0330

    Sample_For_If

--- Quadratic_equation.py
+++ Quadratic_equation.py
@@ -1,2 +1,9 @@
-from colorama import Fore
-print(Fore.GREEN+"ax^2+"+Fore.LIGHTBLUE_EX+"by+"+Fore.LIGHTMAGENTA_EX+"c=0")
+print("ax^2+bx+c=0")
+a=int(input("Please Enter a Number: "))
+b=int(input("Please Enter b Number: "))
+c=int(input("Please Enter c Number: "))
+print("Delta=b^2-4ac")
+
+delta=(b*b)-(4*a*c)
+print ("Delta: " +str(delta))
+
