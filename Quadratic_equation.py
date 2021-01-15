dcfb8f3834eef7469909101b90d1072f1c8688ca
HCodeLand <77481982+hcodeland@users.noreply.github.com>
2021-01-15 13:00:08 +0330

    Quadratic_equation

--- Quadratic_equation.py
+++ Quadratic_equation.py
@@ -1,9 +1,19 @@
-print("ax^2+bx+c=0")
-a=int(input("Please Enter a Number: "))
-b=int(input("Please Enter b Number: "))
-c=int(input("Please Enter c Number: "))
-print("Delta=b^2-4ac")
-
+from colorama import Fore
+import math
+print(Fore.LIGHTRED_EX+"ax^2+"+Fore.LIGHTCYAN_EX+"bx+"+Fore.LIGHTGREEN_EX+"c=0")
+a=int(input(Fore.LIGHTYELLOW_EX+"Please Enter a number:"))
+b=int(input(Fore.LIGHTYELLOW_EX+"Please Enter b number:"))
+c=int(input(Fore.LIGHTYELLOW_EX+"Please Enter c number:"))
+print(Fore.LIGHTMAGENTA_EX+"Delta=b^2-4ac")
 delta=(b*b)-(4*a*c)
-print ("Delta: " +str(delta))
-
+print(Fore.LIGHTWHITE_EX+str(delta))
+if delta>0:
+   x1=((-1*b)-math.sqrt(delta))/(2+a)
+   x2=((-1*b)+math.sqrt(delta))/(2+a)
+   print(Fore.CYAN+"X1: " + str(x1) + Fore.GREEN+" X2: " +str(x2))
+elif delta==0:
+   print(Fore.LIGHTCYAN_EX+"x=-b/2a")
+   x=(-1*b)/(2+a)
+   print(Fore.YELLOW+str(x))
+else:
+   print(Fore.RED+"No Root")
